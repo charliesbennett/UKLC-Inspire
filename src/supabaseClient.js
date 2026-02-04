078968955983da2e664e835cc8c7c773601dd4c3
charliesbennett <charliesb23@gmail.com>
2026-02-04 20:47:36 +0000

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,12 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Replace with your actual credentials
-```javascript
 const supabaseUrl = 'https://szhbpfecthwwwghhtsaq.supabase.co'
-```
-```javascript
+
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6aGJwZmVjdGh3d3dnaGh0c2FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMjg3NjEsImV4cCI6MjA4NTcwNDc2MX0.pKN483Ng4BGgYKizuGGWtpynQor-29RSw8uGOe8dQKs'
-```
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
